f46ece8fdfc7dc02b5ee3d03ba0621bda81db414
Jake Miller <jake0miller@gmail.com>
2019-05-08 19:40:42 -0600

    Add more tests in a separate file and code health and height

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -3,17 +3,20 @@ class BinarySearchTree
 
   def initialize
     @root = nil
+    @num_nodes = 0
   end
 
   def insert(key, title)
     return 0 if set_root_node_if_nil?(key,title)
     return nil if @root.insert(key, title).nil?
+    @num_nodes += 1
     return depth_of(key)
   end
 
   def set_root_node_if_nil?(key, title)
     if @root.nil?
       @root = Node.new(key, title)
+      @num_nodes += 1
       return true
     end
   end
@@ -67,5 +70,65 @@ class BinarySearchTree
 
   def sort
     @root.sort
+  end
+
+  def health(depth)
+    nodes = get_all_nodes_at_depth(depth)
+    results = []
+    nodes.each do |ele|
+      num_nodes_below = count_nodes_below_node(ele)
+      results << [ele.key, num_nodes_below, (100 * num_nodes_below.to_f / @num_nodes).to_i]
+    end
+    results
+  end
+
+  def enqueue(queue, cur_node)
+    queue.push(cur_node.left) if cur_node.left != nil
+    queue.push(cur_node.right) if cur_node.right != nil
+    cur_node = queue.shift
+    [queue, cur_node]
+  end
+
+  def count_nodes_below_node(node)
+    cur_node = node
+    queue = []
+    count_nodes = 0
+    while cur_node != nil
+      queue, cur_node = enqueue(queue, cur_node)
+      count_nodes += 1
+    end
+    count_nodes
+  end
+
+  def get_all_nodes_at_depth(depth)
+    nodes = []
+    queue = []
+    cur_node = @root
+    while cur_node != nil
+      nodes.push(cur_node) if depth_of(cur_node.key) == depth
+      queue, cur_node = enqueue(queue, cur_node)
+    end
+    nodes
+  end
+
+  def leaves
+    leaf_count = 0
+    queue = []
+    cur_node = @root
+    while cur_node != nil
+      leaf_count += 1 if cur_node.left == nil && cur_node.right == nil
+      queue, cur_node = enqueue(queue, cur_node)
+    end
+    leaf_count
+  end
+
+  def height
+    get_sub_height(@root)
+  end
+
+  def get_sub_height(cur_node)
+    left_height = cur_node.left ? get_sub_height(cur_node.left) : 0
+    right_height = cur_node.right ? get_sub_height(cur_node.right) : 0
+    1 + [left_height, right_height].max
   end
 end
---
+++ test/health_test.rb
@@ -0,0 +1,43 @@
+require 'pry'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/binary_search_tree'
+require './lib/node'
+
+class HealthTest < MiniTest::Test
+  def setup
+    @tree = BinarySearchTree.new
+    @tree.insert(98, "Animals United")
+    @tree.insert(58, "Armageddon")
+    @tree.insert(36, "Bill & Ted's Bogus Journey")
+    @tree.insert(93, "Bill & Ted's Excellent Adventure")
+    @tree.insert(86, "Charlie's Angels")
+    @tree.insert(38, "Charlie's Country")
+    @tree.insert(69, "Collateral Damage")
+  end
+
+  def test_it_returns_array
+    assert_instance_of Array, @tree.health(0)
+  end
+
+  def test_return_array_first_elements
+    assert_equal 98, @tree.health(0)[0][0]
+    assert_equal 58, @tree.health(1)[0][0]
+    assert_equal 36, @tree.health(2)[0][0]
+    assert_equal 93, @tree.health(2)[1][0]
+  end
+
+  def test_full_return_array
+    assert_equal [[98, 7, 100]], @tree.health(0)
+    assert_equal [[58, 6, 85]], @tree.health(1)
+    assert_equal [[36, 2, 28], [93, 3, 42]], @tree.health(2)
+  end
+
+  def test_leaves
+    assert_equal 2, @tree.leaves
+  end
+
+  def test_height
+    assert_equal 5, @tree.height
+  end
+end
